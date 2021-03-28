f98a8056d2bbfb6d232384055deab66cde442695
blonde-eileen <eliana.monteleone@virgilio.it>
2021-03-28 15:58:04 +0200

    Collegamento firebase + creazione di due Activity come prototipo per il login.

---
+++ app/src/main/java/com/soundbliss/EmailPasswordActivity.kt
@@ -0,0 +1,96 @@
+package com.soundbliss
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+ class EmailPasswordActivity : Activity() {
+    // declare auth for FireBase with "lateinit" that has to be initialized on constructor
+    private lateinit var auth: FirebaseAuth
+
+
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //initialize auth of Firebase
+        auth = Firebase.auth
+
+    }
+
+    //check user on start
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+
+    private fun createAccount(email: String, password: String) {
+        //create a new user
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+
+    }
+//Sign in with email and password
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun sendEmailVerification() {
+        // [START send_email_verification]
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                // Email Verification sent
+                task.isComplete
+            }
+
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    private fun reload() {
+
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+}
--- app/src/main/java/com/soundbliss/MainActivity.kt
+++ app/src/main/java/com/soundbliss/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.soundbliss
 
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.UserHandle
+import com.google.firebase.ktx.Firebase
+import com.soundbliss.EmailPasswordActivity;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+ 
+
 }
