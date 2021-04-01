1dced01249f3ee7b7c346065079de162599c50ed
fernandoedelstein <fmedelsteinfernand@studenti.uninsubria.it>
2021-04-01 11:46:44 +0200

    LogIn / SignIn Form Modifications part 4

--- app/src/main/java/com/soundbliss/Fragments/SignUp.kt
+++ app/src/main/java/com/soundbliss/Fragments/SignUp.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import com.soundbliss.R
 
 class SignUp : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
