64bc3478c81b1f12170a546e43ebc290d4270a85
fernandoedelstein <fmedelsteinfernand@studenti.uninsubria.it>
2021-04-20 15:37:55 +0200

    Post Activity, Post Fragment

--- app/src/main/java/com/soundbliss/PostActivity.kt
+++ app/src/main/java/com/soundbliss/PostActivity.kt
@@ -2,10 +2,23 @@ package com.soundbliss
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView
+import kotlinx.android.synthetic.main.activity_post.*
 
 class PostActivity : AppCompatActivity() {
+
+    lateinit var description : SocialAutoCompleteTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
+        val close : ImageView = findViewById(R.id.close)
+        val imageAdded : ImageView = findViewById(R.id.image_added)
+        val textView : TextView = findViewById(R.id.post)
+        description = findViewById(R.id.description)
+
     }
 }
