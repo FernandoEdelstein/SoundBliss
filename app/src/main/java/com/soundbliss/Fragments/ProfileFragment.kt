1e20b9ef30f3e9299ea51207d700366637670012
Eliana <emonteleone1@studenti.uninsubria.it>
2021-04-24 11:19:58 +0200

    eliminazione profile activity e sostituzione con profile Fragment

--- app/src/main/java/com/soundbliss/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/soundbliss/Fragments/ProfileFragment.kt
@@ -1,20 +1,88 @@
 package com.soundbliss.Fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.soundbliss.R
+import com.theartofdev.edmodo.cropper.CropImage
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 
 
 class ProfileFragment : Fragment() {
+    private val TAG = "EditProfileFragment"
+
+    private val mProfilePhoto: ImageView? = null
+    private lateinit var name: TextView
+    private lateinit var lastName : TextView
+
+    private lateinit var auth: FirebaseAuth
+    private var databaseReference: DatabaseReference? = null
+    private lateinit var database : FirebaseDatabase
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = Firebase.database("https://soundbliss-8ba73-default-rtdb.europe-west1.firebasedatabase.app").reference;
+        databaseReference?.child("rvmF9RWAXzbjgg5FHEkaX9oHLDt1")
+
+
+
+        val user = auth.currentUser
+        val userReference = databaseReference?.child(user?.uid!!)
+
+
+        userReference?.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                name = view.findViewById(R.id.display_name)
+                lastName = view.findViewById(R.id.display_lastname)
+                name.text=  snapshot.child("name").value?.toString()
+                lastName.text=  snapshot.child("lastname").value?.toString()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+                TODO("Not yet implemented")
+            }
+        })
+
+        return view
     }
 
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
